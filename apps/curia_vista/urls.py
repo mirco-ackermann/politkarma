ac490f7f89f6968b02c60ba3dcbf73b4df5ba77c
Mirco Ackermann <mirco@megaira.physik.uzh.ch>
2018-02-09 15:11:39 +0100

    Fix "no such table" error in travis
    When doing 'flush', 'makemigrations' and 'migrate', tables might
    not be in the database yet, but the modules imported by
    apps/curia_vista/urls.py expect them to be there. Don't import
    and set these urls if we are doing one of these operations that
    might occur when the database has not yet been setup.
    
    This commit should fix the errors in travis and additionally make
    it a lot easier to set up a clean build.

--- apps/curia_vista/urls.py
+++ apps/curia_vista/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import url
 from django.views.generic.list import ListView
 
-from apps.curia_vista.views import *
 
-urlpatterns = [
+import sys
+
+if True in [x in sys.argv for x in ("makemigrations", "migrate", "flush")]:
+  print("urls.py: Not importing things that break makemigrations, migrate, or flush")
+  urlpatterns = []
+else:
+  print("urls.py: importing stuff that breaks if the database is not yet setup.")
+  from apps.curia_vista.views import *
+  urlpatterns = [
     url(r'^department/$', ListView.as_view(context_object_name='departments',
                                            queryset=Department.objects.order_by('-name'))),
     url(r'^party/$', PartyView.as_view(), name='party'),
     url(r'^vote-helper/$', VoteView.as_view(), name='vote'),
 
-]
+  ]
